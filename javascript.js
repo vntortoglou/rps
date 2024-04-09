8d8ac033d75e20714b66bc04b9fa7b8d56c5679f
vntortoglou <vntortoglou@gmail.com>
2024-04-09 17:47:39 +0300

    Finished project

--- javascript.js
+++ javascript.js
@@ -1,36 +1,84 @@
-function playRound(playerSelection, computerSelection) {
+// Variables from HTML
 
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const display = document.querySelector(`h2`);
+const pscore = document.getElementById(`pscore`);
+const cscore = document.getElementById(`cscore`);
+
+
+
+// Global Variables
+
+let playerWins = 0;
+let computerWins = 0;
+
+
+
+
+
+
+// Round rules
+function playRound(playerSelection, computerSelection) {
+ 
 if (playerSelection === "rock" && computerSelection === "rock"){
-    return `It's DRAW!!! Both picked ROCK`
+    display.textContent = "It's DRAW!!! Both picked ROCK";
   }
 else if (playerSelection ==="rock" && computerSelection ==="paper"){
-    return `You LOSE!!! PAPER beats ROCK`
+    display.textContent = `You LOSE!!! PAPER beats ROCK`;
+     computerWins++;
+     cscore.textContent = computerWins;
 }
 else if (playerSelection ==="rock" && computerSelection ==="scissors"){
-        return `YOU WIN!!! ROCK beats SCISSORS`
+    display.textContent = `YOU WIN!!! ROCK beats SCISSORS`;
+     playerWins++;
+     pscore.textContent = playerWins;
     }
 else if (playerSelection ==="paper" && computerSelection ==="rock"){
-        return `You WIN!!! PAPER beats ROCK`
+    display.textContent = `You WIN!!! PAPER beats ROCK`;
+     playerWins++;
+     pscore.textContent = playerWins;
     }
 else if (playerSelection ==="paper" && computerSelection ==="paper"){
-        return `It's DRAW!!! Both picked PAPER`
+    display.textContent = `It's DRAW!!! Both picked PAPER`
     }
 else if (playerSelection ==="paper" && computerSelection ==="scissors"){
-        return `You LOSE!!! SCISSORS beats PAPER`
+    display.textContent = `You LOSE!!! SCISSORS beats PAPER`;
+     computerWins++;
+     cscore.textContent = computerWins;
     }
-
 else if (playerSelection ==="scissors" && computerSelection ==="rock"){
-        return `You LOSE!!! ROCK beats PAPER`
+    display.textContent = `You LOSE!!! ROCK beats PAPER`;
+     computerWins++;
+     cscore.textContent = computerWins;
     }
-
 else if (playerSelection ==="scissors" && computerSelection ==="paper"){
-        return `You WIN!!! SCISSORS beats PAPER`
+    display.textContent = `You WIN!!! SCISSORS beats PAPER`;
+     playerWins++;
+     pscore.textContent = playerWins;
+    }
+else if (playerSelection ==="scissors" && computerSelection ==="scissors"){
+    display.textContent = `It's DRAW!!! You both picked SCISSORS`;
     }
 
-else if (playerSelection ==="scissors" && computerSelection ==="scissors"){
-        return `It's DRAW!!! You both picked SCISSORS`
+    if (playerWins === 5) {
+        display.textContent = "You win!";
+    } else if (computerWins === 5) {
+        display.textContent = "Yoshi wins!";
     }
-}
+
+    updateScores();
+};
+
+
+
+function updateScores() {
+    pscore.textContent = playerWins;
+    cscore.textContent = computerWins;
+    }
+
+// Computer's Choice
 
 let getComputerChoice = () => {
     let randomNumber = Math.floor(Math.random() *3);
@@ -43,14 +91,42 @@ let getComputerChoice = () => {
           return'scissors';
 }
 }
-    
-
-const playerSelection = prompt(`Choose Rock, Paper or Scissors`).toLowerCase();
-if(playerSelection !==`rock` && playerSelection !==`paper` && playerSelection !==`scissors`) {console.log(`Please try again`)}
 
 
 
-  const computerSelection = getComputerChoice();
+rock.addEventListener('click', function(){
+    playerSelection = "rock";
+    return playRound(playerSelection, getComputerChoice());
+});
 
 
-  console.log(playRound(playerSelection, computerSelection))
+paper.addEventListener('click', function(){
+    playerSelection = "paper";
+    return playRound(playerSelection, getComputerChoice());
+});
+
+
+scissors.addEventListener('click', function(){
+    playerSelection = "scissors";
+    return playRound(playerSelection, getComputerChoice());                                    
+});
+
+
+
+function updateScores() {
+    pscore.textContent = playerWins;
+    cscore.textContent = computerWins;
+
+    if (playerWins === 5 || computerWins === 5) {
+        playerWins = 0;
+        computerWins = 0;
+        pscore.textContent = playerWins;
+        cscore.textContent = computerWins;
+
+        if (playerWins === 5) {
+            display.textContent = "You win!";
+        } else if (computerWins === 5) {
+            display.textContent = "You lose!";
+        }
+    }
+}
